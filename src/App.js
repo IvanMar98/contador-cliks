67d02244bf46a4ba7328b2974178d515dc6cb1bd
Ivan Martinez <201720533@tese.edu.mx>
2023-01-02 19:39:21 -0600

    agregando logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import logo from './img/freecodecamp-logo.png'
 function App() {
   return (
     <div className="App">
-      
+      <div className='container-logo'>
+        <img className='logo' src={logo} />
+      </div>
+      <div className='container-counter'>
+        
+      </div>
     </div>
   );
 }
